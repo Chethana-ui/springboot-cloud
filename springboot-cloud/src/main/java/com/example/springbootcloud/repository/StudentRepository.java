822f047bdd1eddad15c143371f71b6f13d649ba8
Chethana-ui <142392438+Chethana-ui@users.noreply.github.com>
2023-10-20 11:06:53 +0530

    Delete springboot-cloud directory

--- springboot-cloud/src/main/java/com/example/springbootcloud/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcloud.repository;
-
-
-import com.example.springbootcloud.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
